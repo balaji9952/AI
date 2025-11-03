84ee525ea2fbe2f8ae377a76c3c9921748579264
balaji9952 <balaji9952@mountzion.ac.in>
2025-11-03 23:39:16 +0530

    Create Q4.py

---
+++ Q4.py
@@ -0,0 +1,85 @@
+import math
+
+def print_board(board):
+    for i in range(3):
+        print(board[i][0], board[i][1], board[i][2])
+
+def evaluate(board):
+    for row in range(3):
+        if board[row][0] == board[row][1] == board[row][2]:
+            if board[row][0] == 'X':
+                return 10
+            elif board[row][0] == 'O':
+                return -10
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col]:
+            if board[0][col] == 'X':
+                return 10
+            elif board[0][col] == 'O':
+                return -10
+    if board[0][0] == board[1][1] == board[2][2]:
+        if board[0][0] == 'X':
+            return 10
+        elif board[0][0] == 'O':
+            return -10
+    if board[0][2] == board[1][1] == board[2][0]:
+        if board[0][2] == 'X':
+            return 10
+        elif board[0][2] == 'O':
+            return -10
+    return 0
+
+def is_moves_left(board):
+    for row in board:
+        if '_' in row:
+            return True
+    return False
+
+def minimax(board, depth, is_max):
+    score = evaluate(board)
+    if score == 10 or score == -10:
+        return score
+    if not is_moves_left(board):
+        return 0
+    if is_max:
+        best = -math.inf
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == '_':
+                    board[i][j] = 'X'
+                    best = max(best, minimax(board, depth + 1, not is_max))
+                    board[i][j] = '_'
+        return best
+    else:
+        best = math.inf
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == '_':
+                    board[i][j] = 'O'
+                    best = min(best, minimax(board, depth + 1, not is_max))
+                    board[i][j] = '_'
+        return best
+
+def find_best_move(board):
+    best_val = -math.inf
+    best_move = (-1, -1)
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == '_':
+                board[i][j] = 'X'
+                move_val = minimax(board, 0, False)
+                board[i][j] = '_'
+                if move_val > best_val:
+                    best_move = (i, j)
+                    best_val = move_val
+    return best_move
+
+board = [
+    ['X', 'O', 'X'],
+    ['O', 'O', '_'],
+    ['_', '_', 'X']
+]
+
+print_board(board)
+best_move = find_best_move(board)
+print("Best Move:", best_move)
