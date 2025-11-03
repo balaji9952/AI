d979cd9a4f2f725f822dd10666d8e561baad59d2
balaji9952 <balaji9952@mountzion.ac.in>
2025-11-03 23:38:22 +0530

    Update Q5.py

--- Q5.py
+++ Q5.py
@@ -1,85 +1,42 @@
-import math
+from queue import PriorityQueue
 
-def print_board(board):
-    for i in range(3):
-        print(board[i][0], board[i][1], board[i][2])
+def a_star(start, goal, graph, h):
+    open_set = PriorityQueue()
+    open_set.put((0, start))
+    came_from = {}
+    g_score = {node: float('inf') for node in graph}
+    g_score[start] = 0
+    f_score = {node: float('inf') for node in graph}
+    f_score[start] = h[start]
+    while not open_set.empty():
+        current = open_set.get()[1]
+        if current == goal:
+            path = []
+            while current in came_from:
+                path.append(current)
+                current = came_from[current]
+            path.append(start)
+            path.reverse()
+            return path
+        for neighbor, cost in graph[current]:
+            temp_g = g_score[current] + cost
+            if temp_g < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g
+                f_score[neighbor] = temp_g + h[neighbor]
+                open_set.put((f_score[neighbor], neighbor))
+    return None
 
-def evaluate(board):
-    for row in range(3):
-        if board[row][0] == board[row][1] == board[row][2]:
-            if board[row][0] == 'X':
-                return 10
-            elif board[row][0] == 'O':
-                return -10
-    for col in range(3):
-        if board[0][col] == board[1][col] == board[2][col]:
-            if board[0][col] == 'X':
-                return 10
-            elif board[0][col] == 'O':
-                return -10
-    if board[0][0] == board[1][1] == board[2][2]:
-        if board[0][0] == 'X':
-            return 10
-        elif board[0][0] == 'O':
-            return -10
-    if board[0][2] == board[1][1] == board[2][0]:
-        if board[0][2] == 'X':
-            return 10
-        elif board[0][2] == 'O':
-            return -10
-    return 0
+graph = {
+    'A': [('B', 1), ('C', 3)],
+    'B': [('D', 3), ('E', 1)],
+    'C': [('F', 5)],
+    'D': [('G', 2)],
+    'E': [('G', 1)],
+    'F': [('G', 2)],
+    'G': []
+}
 
-def is_moves_left(board):
-    for row in board:
-        if '_' in row:
-            return True
-    return False
-
-def minimax(board, depth, is_max):
-    score = evaluate(board)
-    if score == 10 or score == -10:
-        return score
-    if not is_moves_left(board):
-        return 0
-    if is_max:
-        best = -math.inf
-        for i in range(3):
-            for j in range(3):
-                if board[i][j] == '_':
-                    board[i][j] = 'X'
-                    best = max(best, minimax(board, depth + 1, not is_max))
-                    board[i][j] = '_'
-        return best
-    else:
-        best = math.inf
-        for i in range(3):
-            for j in range(3):
-                if board[i][j] == '_':
-                    board[i][j] = 'O'
-                    best = min(best, minimax(board, depth + 1, not is_max))
-                    board[i][j] = '_'
-        return best
-
-def find_best_move(board):
-    best_val = -math.inf
-    best_move = (-1, -1)
-    for i in range(3):
-        for j in range(3):
-            if board[i][j] == '_':
-                board[i][j] = 'X'
-                move_val = minimax(board, 0, False)
-                board[i][j] = '_'
-                if move_val > best_val:
-                    best_move = (i, j)
-                    best_val = move_val
-    return best_move
-
-board = [
-    ['X', 'O', 'X'],
-    ['O', 'O', '_'],
-    ['_', '_', 'X']
-]
-
-print_board(board)
-best_move = find_best_move(board)
-print("Best Move:", best_move)
+h = {'A': 7, 'B': 6, 'C': 5, 'D': 4, 'E': 2, 'F': 1, 'G': 0}
+path = a_star('A', 'G', graph, h)
+print("Path:", path)
